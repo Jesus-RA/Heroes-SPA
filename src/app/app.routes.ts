3d4e5e902216baeb7fe56983415b913c469ca948
Jesus-RA <jesus.ra98@hotmail.com>
2020-11-21 22:01:52 -0600

    Deploying to GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,12 @@ import { HeroeComponent } from './components/heroe/heroe.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 const APP_ROUTES : Routes = [
-    { path: 'home', component: HomeComponent },
+    // { path: 'home', component: HomeComponent },
     { path: 'heroes', component: HeroesComponent},
     { path: 'heroe/:id', component: HeroeComponent },
     { path: 'about', component:AboutComponent },
     { path: 'heroes/search/:termino', component: SearchResultsComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', pathMatch: 'full', redirectTo: 'heroes' },
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
